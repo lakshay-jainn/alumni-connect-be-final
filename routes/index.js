2e864da4c5b7f5ecf66a4d168408f3dcd5e49153
lakshay-jainn <lakshay6690@gmail.com>
2025-01-28 17:31:50 +0530

    added handle-media , and profile upload logic. please check

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,12 @@ import { Router } from "express";
 
 import userRoutes from "./v1/user/index.js"
 import connectionRoutes from "./v1/connection/index.js"
+import mediaRoutes from './v1/handle-media/index.js'
 import adminRoutes from "./v1/admin/index.js"
 import alumni_student_Routes from "./v1/alumni_student/index.js";
 
 import { checkForAuthentication , restrictToOnly } from "../middlewares/auth.js";
-import { seed } from "../services/seed.js";
+// import { seed } from "../services/seed.js";
 const router = Router()
 
 
@@ -19,12 +20,13 @@ router.use("/v1/alumni-student" ,checkForAuthentication, alumni_student_Routes)
 
 router.use("/v1/connection",checkForAuthentication , connectionRoutes)
 
+router.use("/v1/handle-media",checkForAuthentication , mediaRoutes)
 // All can access this route
 router.use("/v1/user" , userRoutes)
 
 
-//Seeding database
-router.post("/v1/seed" , seed)
+// //Seeding database
+// router.post("/v1/seed" , seed)
 
 
 export default router
