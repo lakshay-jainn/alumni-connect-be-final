4f02f6adb01b2e4ece838f025942a660e4e6c4e1
Rish <rishk219@gmail.com>
2025-01-31 01:22:07 +0530

    added_full_logic_posts_refractoring_needed

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ import userRoutes from "./v1/user/index.js"
 import connectionRoutes from "./v1/connection/index.js"
 import adminRoutes from "./v1/admin/index.js"
 import alumni_student_Routes from "./v1/alumni_student/index.js";
-
+import postRoutes from "./v1/post/index.js"
 import { checkForAuthentication , restrictToOnly } from "../middlewares/auth.js";
 import { seed } from "../services/seed.js";
 const router = Router()
@@ -16,8 +16,9 @@ router.use("/v1/admin",checkForAuthentication, restrictToOnly("ADMIN"),adminRout
 
 // Both student and alumni 
 router.use("/v1/alumni-student" ,checkForAuthentication, alumni_student_Routes)
-
 router.use("/v1/connection",checkForAuthentication , connectionRoutes)
+router.use("/v1/posts", checkForAuthentication,postRoutes)
+
 
 // All can access this route
 router.use("/v1/user" , userRoutes)
---
+++ services/postService.js
@@ -0,0 +1,184 @@
+import {prisma} from "../libs/prisma.js";
+
+export async function createPost (userId,content,caption) {
+    return await prisma.post.create({
+        data: {
+            content,
+            userId,
+            caption
+        },
+        // include: {
+        //   user: true  if u ever need user details at the time of creation lakshay
+        // }
+    })
+}
+
+export async function getPosts (userId,skip=0,take=15) {
+    const posts =  await prisma.post.findMany({
+        skip,
+        take,
+        include: {
+          user: true,
+          likes: {
+            where: {
+              userId
+            },
+            select: {
+              userId: true
+            }
+          },
+          comments: {
+            include: {
+              user: true,
+              likes: true
+            }
+          }
+        },
+        orderBy: {
+          createdAt: 'desc', // Sorting baby 
+        },
+      });
+      return posts.map((post) => ({
+        ...post,
+        isLiked: post.likes.length > 0
+      }))
+}
+
+
+export async function likeDislikePost (userId,postId) {
+  const like = await prisma.postLike.findFirst({
+    where: {
+      userId,
+      postId
+    }
+  })
+
+  if(like) {
+    await prisma.postLike.delete({
+      where: {
+        id: like.id
+      }
+    })
+
+    await prisma.post.update({
+      where: {
+        id: postId,
+      },
+      data: {
+        likesCount: {
+          decrement: 1
+        }
+    }})
+
+    return {
+      isLiked: false
+    }
+  }
+
+  await prisma.postLike.create({
+    data: {
+      userId,
+      postId
+    }
+  })
+
+  await prisma.post.update({
+    where: {
+      id: postId,
+    },
+    data: {
+      likesCount: {
+        increment: 1
+      }
+  }})
+
+  return {
+    isLiked: true
+  }
+}
+
+export async function createComment(userId , postId , content) {
+  return await prisma.comment.create({
+    data: {
+      userId,
+      postId,
+      comment: content
+    }
+  })
+}
+
+export async function getComments(userId,postId,skip= 0,take=3) {
+  const comments = await prisma.comment.findMany({
+    where:{
+      postId
+    },
+    include: {
+      user: true,
+      likes: {
+        where: {
+          userId
+        },
+        select: {
+          userId: true
+        }
+      }
+    }
+  })
+
+  return comments.map((comment) => ({
+    ...comment,
+    isLiked: comment.likes.length > 0
+  }))
+}
+
+export async function likeDislikeComment(userId, commentId){
+  const like = await prisma.commentLike.findFirst({
+    where: {
+      userId,
+      commentId
+    }
+  })
+
+  if(like) {
+    await prisma.commentLike.delete({
+      where: {
+        id: like.id
+      }
+    })
+
+    await prisma.comment.update({
+      where: {
+        id: commentId,
+      },
+      data: {
+        likesCount: {
+          decrement: 1
+        }
+    }})
+
+    return {
+      isLiked: false
+    }
+  }
+
+  await prisma.commentLike.create({
+    data: {
+      userId,
+      commentId
+    }
+  })
+
+  await prisma.comment.update({
+    where: {
+      id: commentId,
+    },
+    data: {
+      likesCount: {
+        increment: 1
+      }
+  }})
+
+  return {
+    isLiked: true
+  }
+}
